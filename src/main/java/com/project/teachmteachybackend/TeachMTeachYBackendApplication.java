af2050956edc3d8598f1b846fdb56467620c7249
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-03-12 23:36:15 +0300

    Initial commit

---
+++ src/main/java/com/project/teachmteachybackend/TeachMTeachYBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.teachmteachybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachMTeachYBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachMTeachYBackendApplication.class, args);
+	}
+
+}
