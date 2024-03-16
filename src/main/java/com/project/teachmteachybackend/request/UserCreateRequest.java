04531c9466bdc15620419bcef122d999515c02e7
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-03-17 00:13:50 +0300

    User created validation added

--- src/main/java/com/project/teachmteachybackend/request/UserCreateRequest.java
+++ src/main/java/com/project/teachmteachybackend/request/UserCreateRequest.java
@@ -1,15 +1,13 @@
 package com.project.teachmteachybackend.request;
 
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class UserCreateRequest {
     @NotNull
+    @NotEmpty
     @NotBlank(message = "Kullanıcı adı boş olamaz")
     private String username;
 
