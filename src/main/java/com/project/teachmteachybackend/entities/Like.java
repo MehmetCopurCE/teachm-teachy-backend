b34457080a3f9731aa7e6ba25a7ffab077072371
Mehmet Copur <111346774+MehmetCopurCE@users.noreply.github.com>
2024-03-18 13:19:17 +0300

    Empty Like class created for Efşan :)

---
+++ src/main/java/com/project/teachmteachybackend/entities/Like.java
@@ -0,0 +1,9 @@
+package com.project.teachmteachybackend.entities;
+
+public class Like {
+    //Burası bi like objesi
+    //Database şemasında like objesinde hangi parametreleri görüyorsan burada o özelliklerin olması lazım diğer entitiy lere bakabilirsin
+    //Bu sayfanın bir entity olduğunu belirt. Gerekli annotationları eklemeyi unutma @Data,@Entity ve diğerleri gibi
+    //Tablo adı p_like olsun buna bi dikkat etsen iyi olur. Çünkü database kısmında like adında bir tablo oluşturulamıyor
+    //büyük ihtimalle başka bi şeye indexli o
+}
